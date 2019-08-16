645b560ec80772f790ab5d4b9f58a2920e70e64a
Jasper Stam <jasper.stam@philips.com>
2019-08-16 10:20:11 +0200

    Initial barebone project

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody2D Rigidbody;
+
+    public BoxCollider2D Player;
+
+    public float moveStrength = 1;
+    public float jumpStrength = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D[] hits = new RaycastHit2D[1];
+        Player.Raycast(Vector2.down, hits);
+        if (hits.Length > 0 && hits[0].distance < 1.5) {
+            Debug.Log(hits[0].distance);
+            Rigidbody.AddForce(Vector2.right * Input.GetAxis("Horizontal") * moveStrength);
+            Rigidbody.AddForce(Vector2.up * Input.GetAxis("Jump") * jumpStrength);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public Rigidbody2D Rigidbody;
+
+    public Animator Animator;
+
+    public Transform AnimatorTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Rigidbody.velocity.x != 0) {
+            Animator.SetInteger("State", 1);
+            if (Rigidbody.velocity.x < 0) {
+                AnimatorTransform.localScale = new Vector3(-1, 1, 1);
+            } else {
+                AnimatorTransform.localScale = new Vector3(1, 1, 1);
+            }
+        } else {
+            Animator.SetInteger("State", 0);
+        }
+    }
+}
