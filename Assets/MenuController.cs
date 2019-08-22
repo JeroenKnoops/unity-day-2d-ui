5497798a22dee391eee1af1e1ba1596087d2ede9
Jeroen Knoops <jeroen.knoops@gmail.com>
2019-08-22 13:47:24 +0200

    add function

--- Assets/MenuController.cs
+++ Assets/MenuController.cs
@@ -4,4 +4,19 @@ public class MenuController : MonoBehaviour
 {
   public GameObject MenuPanel;
   public GameObject ButtonPrefab;
+
+  public void Start()
+  {
+    var newButton = Instantiate(ButtonPrefab);
+    newButton.transform.SetParent(MenuPanel.transform);
+
+    var script = newButton.GetComponent<MenuItem>();
+    script.Initialize("Close Menu", CloseMenuPlz);
+
+  }
+
+  private void CloseMenuPlz()
+  {
+    Debug.Log("now close the menu plzkthxbye");
+  }
 }
