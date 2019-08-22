677901749ed695a3cb950393509ba1994f5f2bca
Jeroen Knoops <jeroen.knoops@gmail.com>
2019-08-22 13:31:34 +0200

    Add handler in code

--- Assets/MenuItem.cs
+++ Assets/MenuItem.cs
@@ -1,15 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MenuItem : MonoBehaviour
 {
+    private Action _buttonAction;
+
     public Text ButtonText;
+
+    public void Initialize(string text, Action action) {
+        _buttonAction = action;
+        ButtonText.text = text;
+    }
 
     public void MenuButtonClicked()
     {
-
+        _buttonAction();
     }
 
 }
